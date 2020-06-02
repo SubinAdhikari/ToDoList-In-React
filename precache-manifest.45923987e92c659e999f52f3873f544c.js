31d6ae31b54d3bbc43407cde692963519fb445e7
SubinAdhikari <subinadhikari61@gmail.com>
2020-06-02 10:57:09 +0545

    Updates

---
+++ precache-manifest.45923987e92c659e999f52f3873f544c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4b595013cac4421deb066a0fb189d50",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "fc5b87dd85243e85383e",
+    "url": "/react-gh-pages/static/css/main.c6aad9a7.chunk.css"
+  },
+  {
+    "revision": "fc6deaa18904a427f7f9",
+    "url": "/react-gh-pages/static/js/2.182eb09d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-gh-pages/static/js/2.182eb09d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc5b87dd85243e85383e",
+    "url": "/react-gh-pages/static/js/main.3bdb6fc1.chunk.js"
+  },
+  {
+    "revision": "2f2b0afc87cfd8d7b2b3",
+    "url": "/react-gh-pages/static/js/runtime-main.14eb7928.js"
+  }
+]);
